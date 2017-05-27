420ef9126506bca804ed6641d6fc417bfd224950
gill0381 <gill0381@algonquinlive.com>
2017-05-27 12:34:53 -0400

    edited js file

---
+++ js/looper.js
@@ -0,0 +1,15 @@
+var letter;
+while (!letter) {
+       letter = prompt("Enter a letter please");
+}
+
+    var i = 0;
+    for (var i =0; i < 10; i +=1) {
+    
+                    
+    var k = 0;                
+    for (var k = 0; k <= i; k += 1) {
+         document.write(letter);            
+        }
+document.write("<br>");  
+    }
